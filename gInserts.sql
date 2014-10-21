394d80d45faeddfef4ac336100e3fffdff1958dc
Gary Healy <gary.healy2@student.dit.ie>
2014-10-21 14:57:38 +0100

    Initial Version

---
+++ gInserts.sql
@@ -0,0 +1,12 @@
+--Staff Table Inserts
+INSERT INTO staff (staff_id, staff_name)
+VALUES
+(1234, "Gary Healy");
+
+INSERT INTO staff (staff_id, staff_name)
+VALUES
+(1235, "Stefan Burke");
+
+INSERT INTO staff (staff_id, staff_name)
+VALUES
+(1236, "Francis Marron");
