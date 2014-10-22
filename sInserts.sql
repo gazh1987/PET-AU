6aab27bb252dce0991c318693458d0a546ac6a35
stefanburke <c12723809@mydit.ie>
2014-10-22 10:48:48 +0100

    Stefan Inserts

---
+++ sInserts.sql
@@ -0,0 +1,7 @@
+--Stock Inserts
+INSERT INTO Stock_order (order_no, supplier_id, quantity, cost_price, staff_id, order_date) VALUES (4000, 2001, 30, 05.00, 1234, 10-Oct-2014);
+INSERT INTO Stock_order (order_no, supplier_id, quantity, cost_price, staff_id, order_date) VALUES (4001, 2002, 50, 60.00, 1235, 12-Oct-2014);
+INSERT INTO Stock_order (order_no, supplier_id, quantity, cost_price, staff_id, order_date) VALUES (4002, 2003, 30, 05.00, 1236, 15-Oct-2014);
+INSERT INTO Stock_order (order_no, supplier_id, quantity, cost_price, staff_id, order_date) VALUES (4003, 2004, 5, 05.00, 1234, 10-Oct-2014);
+
+--
