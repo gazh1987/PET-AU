0de44af83338b3fe7e7ac5862ca42d44979eaac7
Gary Healy <gary.healy2@student.dit.ie>
2014-10-23 18:29:52 +0100

    New Entries to Stock and Materials Table

--- allInserts.sql
+++ allInserts.sql
@@ -1,4 +1,4 @@
--- Staff Table
+------- Staff Table
 INSERT INTO staff (staff_id, staff_name) VALUES (1234, 'Gary Healy');
 INSERT INTO staff (staff_id, staff_name) VALUES (1235, 'Stefan Burke');
 INSERT INTO staff (staff_id, staff_name) VALUES (1236, 'Francis Marron');
